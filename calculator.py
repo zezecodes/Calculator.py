61a866c4b639833ea6dafb4d068ae5dab44fc90d
Zeze <djangsiness01@gmail.com>
2022-10-28 13:50:28 +0000

    New changes to file

---
+++ calculator.py
@@ -0,0 +1,31 @@
+def addition():
+    sumVal1 = int(input("Please input the first value you want to sum up: "))
+    sumVal2 = int(input("Now input the second: "))
+    addNumber1 = sumVal1
+    addNumber2 = sumVal2
+    
+    return sumVal1 + sumVal2
+    
+def subtraction():
+    diffVal1 = int(input("Please input the first value you want to sum up: "))
+    diffVal2 = int(input("Now input the second: "))
+    subtractNumber1 = diffVal1
+    subtractNumber2 = diffVal2
+
+    return diffVal1 - diffVal2
+
+def division():
+    return print("The quotient is: "+(x/y))
+
+def multiplication():
+    return print("The product is: "+(x*y))
+
+operationInput = input("Please input the operation you want to use on your values(+ _ * /): ")
+if operationInput == "+":    
+    print(addition())
+elif operationInput == "-":
+    print(subtraction())
+elif operationInput == "*":
+    print(multiplication())
+elif operationInput == "/":
+    print(division())
