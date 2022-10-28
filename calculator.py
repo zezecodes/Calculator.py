942f8133a6237fd10c09e453ef75b0698e508081
Zeze <djangsiness01@gmail.com>
2022-10-28 13:59:46 +0000

    new commit new commit

--- calculator.py
+++ calculator.py
@@ -3,8 +3,8 @@ def addition():
     sumVal2 = int(input("Now input the second: "))
     addNumber1 = sumVal1
     addNumber2 = sumVal2
-    
-    return sumVal1 + sumVal2
+
+    return print("The sum is: "+str(sumVal1 + sumVal2))
     
 def subtraction():
     diffVal1 = int(input("Please input the first value you want to sum up: "))
@@ -12,9 +12,14 @@ def subtraction():
     subtractNumber1 = diffVal1
     subtractNumber2 = diffVal2
 
-    return diffVal1 - diffVal2
+    return print("The difference is: "+str(diffVal1 - diffVal2))
 
 def division():
+    diffVal1 = int(input("Please input the first value you want to sum up: "))
+    diffVal2 = int(input("Now input the second: "))
+    subtractNumber1 = diffVal1
+    subtractNumber2 = diffVal2
+
     return print("The quotient is: "+(x/y))
 
 def multiplication():
