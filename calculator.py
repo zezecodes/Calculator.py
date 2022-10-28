1c2bb9e2415977c68b6865ba66e2c2616756a8d1
Zeze <djangsiness01@gmail.com>
2022-10-28 15:26:43 +0000

    file updates

--- calculator.py
+++ calculator.py
@@ -12,17 +12,21 @@ def subtraction():
     subtractNumber1 = diffVal1
     subtractNumber2 = diffVal2
 
-    return print("The difference is: "+str(diffVal1 - diffVal2))
+    return print("The difference is: "+str(diffVal2 - diffVal1))
 
 def division():
-    diffVal1 = int(input("Please input the first value you want to sum up: "))
-    diffVal2 = int(input("Now input the second: "))
-    subtractNumber1 = diffVal1
-    subtractNumber2 = diffVal2
+    quotientVal1 = int(input("Please input the dividend: "))
+    quotientVal2 = int(input("Now input the divisor: "))
+    divisionNumber1 = quotientVal1
+    divisionNumber2 = quotientVal2
 
     return print("The quotient is: "+str(diffVal1 - diffVal2))
 
 def multiplication():
+    diffVal1 = int(input("Please input the value you want to subtract: "))
+    diffVal2 = int(input("Now input the value you want to subtract from: "))
+    subtractNumber1 = diffVal1
+    subtractNumber2 = diffVal2
     return print("The product is: "+(x*y))
 
 operationInput = input("Please input the operation you want to use on your values(+ _ * /): ")
